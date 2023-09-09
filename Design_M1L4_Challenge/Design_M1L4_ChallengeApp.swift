c646079e97ec406d0814a356d2c342359b9ce0d9
Tom Montgomery <montgomery3681@gmail.com>
2023-09-08 22:32:26 -0600

    Initial Commit

---
+++ Design_M1L4_Challenge/Design_M1L4_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_M1L4_ChallengeApp.swift
+//  Design_M1L4_Challenge
+//
+//  Created by tom montgomery on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Design_M1L4_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
